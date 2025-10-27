887b5de16b3875ec81b16c5c20f282f1be008cfc
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-27 21:34:50 +0530

    updated

--- src/main/java/com/substring/chat_app/repositories/RoomRepository.java
+++ src/main/java/com/substring/chat_app/repositories/RoomRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface RoomRepository extends MongoRepository<Room, Long> {
 
-    //get room using roomId
     Room findByRoomId(String roomId);
+
+
 
 }
