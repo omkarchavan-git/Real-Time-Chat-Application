be399489b02c81a74b44c599ad7e1f270a465e42
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-27 21:34:43 +0530

    updated

--- src/main/java/com/substring/chat_app/controllers/RoomController.java
+++ src/main/java/com/substring/chat_app/controllers/RoomController.java
@@ -1,4 +1,34 @@
 package com.substring.chat_app.controllers;
 
+import com.substring.chat_app.entities.Room;
+import com.substring.chat_app.repositories.RoomRepository;
+import com.substring.chat_app.roomService.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class RoomController {
+
+    @Autowired
+    private RoomService roomService;
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    //create room
+    @PostMapping
+    public ResponseEntity<?> createRoom(@RequestBody String roomId){
+
+        if(roomService.findByRoomId(roomId) != null){
+            // the room is already Exists
+            return ResponseEntity.badRequest().body("the room is already exists !");
+
+        }
+        //create new room
+        Room room = new Room();
+        room.setRoomId(roomId);
+        Room savedRoom = roomRepository.save(room);
+
+    }
 }
