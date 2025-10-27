173c31716318726bd844adecb00239414b35db38
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-27 21:35:05 +0530

    updated

--- src/main/java/com/substring/chat_app/serviceImpl/RoomServiceImpl.java
+++ src/main/java/com/substring/chat_app/serviceImpl/RoomServiceImpl.java
@@ -1,10 +1,12 @@
 package com.substring.chat_app.serviceImpl;
 
 import com.substring.chat_app.entities.Room;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import com.substring.chat_app.repositories.RoomRepository;
 import com.substring.chat_app.roomService.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 public class RoomServiceImpl implements RoomService {
@@ -13,8 +15,10 @@ public class RoomServiceImpl implements RoomService {
     @Autowired
     private RoomRepository roomRepository;
 
-    public Room findroombyid(String roomId){
-        Room roombyid = roomRepository.findByRoomId(roomId);
-        return roombyid;
+
+
+    @Override
+    public Room findByRoomId(String roomId) {
+       return roomRepository.findByRoomId(roomId);
     }
 }
