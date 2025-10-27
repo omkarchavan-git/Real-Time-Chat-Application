09774bf90bf343d7c2387e1db9bb989bbeb2d93a
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-27 21:34:56 +0530

    updated

--- src/main/java/com/substring/chat_app/roomService/RoomService.java
+++ src/main/java/com/substring/chat_app/roomService/RoomService.java
@@ -1,4 +1,10 @@
 package com.substring.chat_app.roomService;
 
+import com.substring.chat_app.entities.Room;
+
 public interface RoomService {
+
+    //get room using roomId
+    Room findByRoomId(String roomId);
+
 }
