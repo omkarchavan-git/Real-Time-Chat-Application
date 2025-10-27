c215e84433d667c7e02467e9613c116ed293b7c3
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-27 21:34:27 +0530

    updated

--- src/main/java/com/substring/chat_app/controllers/ChatController.java
+++ src/main/java/com/substring/chat_app/controllers/ChatController.java
@@ -1,4 +1,11 @@
 package com.substring.chat_app.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rooms")
 public class ChatController {
+
+
 }
