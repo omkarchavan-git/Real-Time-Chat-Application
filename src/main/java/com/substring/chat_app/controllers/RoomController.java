0d5703736465a8f3952096b43d73199a2862e6ca
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-28 13:09:23 +0530

    Created Room controller to create room

--- src/main/java/com/substring/chat_app/controllers/RoomController.java
+++ src/main/java/com/substring/chat_app/controllers/RoomController.java
@@ -1,13 +1,17 @@
 package com.substring.chat_app.controllers;
 
+import com.substring.chat_app.entities.Message;
 import com.substring.chat_app.entities.Room;
 import com.substring.chat_app.repositories.RoomRepository;
 import com.substring.chat_app.roomService.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+@RestController
+@RequestMapping("/api/rooms")
 public class RoomController {
 
     @Autowired
@@ -17,7 +21,7 @@ public class RoomController {
     private RoomRepository roomRepository;
 
     //create room
-    @PostMapping
+    @PostMapping()
     public ResponseEntity<?> createRoom(@RequestBody String roomId){
 
         if(roomService.findByRoomId(roomId) != null){
@@ -29,6 +33,38 @@ public class RoomController {
         Room room = new Room();
         room.setRoomId(roomId);
         Room savedRoom = roomRepository.save(room);
+        return ResponseEntity.status(HttpStatus.CREATED).body(room);
 
     }
+
+    // Join room
+    @GetMapping("/{roomId}")
+    public ResponseEntity<?> joinRoom(@PathVariable String roomId){
+        Room room = roomRepository.findByRoomId(roomId);
+        if (room == null)
+        {
+            return ResponseEntity.badRequest().body("Room Not Found");
+        }
+        return ResponseEntity.ok(room);
+    }
+
+
+    //get messages of the room
+
+    @GetMapping("/{roomId}/messages")
+    public ResponseEntity<List<Message>> getMessages (@PathVariable String roomId){
+
+        Room room = roomRepository.findByRoomId(roomId);
+        if (room == null)
+        {
+            return ResponseEntity.badRequest().build();
+        }
+
+
+        List<Message> messages = room.getMessages();
+        return ResponseEntity.ok(messages);
+
+    }
+
+
 }
