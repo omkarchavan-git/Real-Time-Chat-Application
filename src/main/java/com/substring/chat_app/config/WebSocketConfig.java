ee5f828c20a410357aaf2a732534ae8a8188eebd
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-29 16:04:00 +0530

    added methods for the configuration

--- src/main/java/com/substring/chat_app/config/WebSocketConfig.java
+++ src/main/java/com/substring/chat_app/config/WebSocketConfig.java
@@ -1,7 +1,23 @@
 package com.substring.chat_app.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Configuration
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+    }
+
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+    }
 }
