a552e3f6bce7c4d2dcaaca2e42316f439cba221f
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-29 16:00:48 +0530

    Added getter and  setter

--- src/main/java/com/substring/chat_app/entities/Message.java
+++ src/main/java/com/substring/chat_app/entities/Message.java
@@ -14,10 +14,33 @@ public class Message {
     private String content;
     private LocalDateTime timeStamp;
 
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
     public Message(String content, String sender) {
         this.content = content;
         this.sender = sender;
         this.timeStamp = LocalDateTime.now();
-
     }
 }
