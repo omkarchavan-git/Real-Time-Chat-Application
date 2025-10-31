cffd2d53f37302cffb8c765da0b0fe8e6acfb65b
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-31 19:48:29 +0530

    updated with new class

--- chat-app-UI/src/App.jsx
+++ chat-app-UI/src/App.jsx
@@ -1,19 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ChatRoom from "./components/ChatRoom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
+    <Router>
+      <div style={{ textAlign: "center", marginTop: "30px" }}>
+        <h1>💬 Real-Time Group Chat</h1>
 
+        <nav style={{ marginBottom: "20px" }}>
+          <Link to="/room/room1" style={linkStyle}>Room 1</Link>
+          <Link to="/room/room2" style={linkStyle}>Room 2</Link>
+          <Link to="/room/room3" style={linkStyle}>Room 3</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/room/:roomId" element={<ChatRoom />} />
+          <Route path="*" element={<div>Select a room to start chatting.</div>} />
+        </Routes>
       </div>
-
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+const linkStyle = {
+  margin: "0 15px",
+  textDecoration: "none",
+  color: "blue",
+  fontWeight: "bold",
+};
+
+export default App;
