4acdc8b172658d84aff57fe924ea655864a7a729
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-31 20:08:06 +0530

    updated file path

--- chat-app-UI/src/App.jsx
+++ chat-app-UI/src/App.jsx
@@ -1,33 +1,14 @@
+// src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import ChatRoom from "./components/ChatRoom";
+import ChatRoom from "./chatroom/ChatRoom";
 
 function App() {
   return (
-    <Router>
-      <div style={{ textAlign: "center", marginTop: "30px" }}>
-        <h1>💬 Real-Time Group Chat</h1>
-
-        <nav style={{ marginBottom: "20px" }}>
-          <Link to="/room/room1" style={linkStyle}>Room 1</Link>
-          <Link to="/room/room2" style={linkStyle}>Room 2</Link>
-          <Link to="/room/room3" style={linkStyle}>Room 3</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/room/:roomId" element={<ChatRoom />} />
-          <Route path="*" element={<div>Select a room to start chatting.</div>} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <h1 style={{ textAlign: "center" }}>Realtime Chat App</h1>
+      <ChatRoom />
+    </div>
   );
 }
-
-const linkStyle = {
-  margin: "0 15px",
-  textDecoration: "none",
-  color: "blue",
-  fontWeight: "bold",
-};
 
 export default App;
