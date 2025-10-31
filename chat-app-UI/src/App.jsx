bf8d9dfc7bccff661c87a2a511a9a82772dc222a
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-31 20:16:07 +0530

    updated file path

--- chat-app-UI/src/App.jsx
+++ chat-app-UI/src/App.jsx
@@ -1,11 +1,10 @@
-// src/App.jsx
 import React from "react";
 import ChatRoom from "./chatroom/ChatRoom";
 
 function App() {
   return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>Realtime Chat App</h1>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>💬 Real-time Chat Application</h1>
       <ChatRoom />
     </div>
   );
