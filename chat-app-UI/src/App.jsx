f317992008555ec15325fc5a48ebb706c55ce477
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-11-04 19:30:27 +0530

    updated config file

--- chat-app-UI/src/App.jsx
+++ chat-app-UI/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import ChatRoom from "./components/ChatRoom";
 import Dashboard from "./components/Dashboard";
+import ChatRoom from "./components/ChatRoom";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
-      <Route path="/chat" element={<ChatRoom />} />
+      <Route path="/chat/:roomId" element={<ChatRoom />} />
     </Routes>
   );
 }
